ae635bac5b5800267a84f38db88e14bd2ed1716e
Nick Liebmann <53795413+Liebmann5@users.noreply.github.com>
2022-11-24 17:21:21 -0600

    Create Welcome.js

---
+++ React/Welcome.js
@@ -0,0 +1,32 @@
+//Passing a prop to an ES6 class component
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+            { /* Change code below this line */ }
+            <Welcome name="Rock Legend: Bruce Dickinson" />
+            { /* Change code above this line */ }
+        </div>
+    );
+  }
+};
+
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+        <div>
+          { /* Change code below this line */ }
+          <p>Hello, <strong>{this.props.name}</strong>!</p>
+          { /* Change code above this line */ }
+        </div>
+    );
+  }
+};
