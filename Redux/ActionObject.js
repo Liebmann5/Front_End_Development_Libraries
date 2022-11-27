0086167730cd4b9c89e620ebb4c7b820f2c4a65b
Nick Liebmann <53795413+Liebmann5@users.noreply.github.com>
2022-11-27 16:21:09 -0600

    Create ActionObject.js

---
+++ Redux/ActionObject.js
@@ -0,0 +1,6 @@
+// How to declare a Redux Action:
+{/*Writing a Redux action is as simple as declaring an object with a type property. Declare an object action and give it a property type set to the string 'LOGIN'.*/}
+let action = {type: 'LOGIN'}
+
+{/* Think of Redux actions as messengers that deliver information about events happening in your app to the Redux store. The store then conducts the business of
+updating state based on the action that occurred. */}
