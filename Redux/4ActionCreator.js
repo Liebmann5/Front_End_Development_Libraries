acec89d7b8c5a0740261554c5117a731177f4f11
Nick Liebmann <53795413+Liebmann5@users.noreply.github.com>
2022-11-27 16:36:14 -0600

    Create 4ActionCreator.js

---
+++ Redux/4ActionCreator.js
@@ -0,0 +1,9 @@
+const action = {
+  type: 'LOGIN'
+}
+// Define an action creator here:
+{/* An action creator is simply a JavaScript function that returns an action */}
+{/* In other words, action creators[f(n)] / create objects[return event;] / that represent action events */}
+function actionCreator() { 
+  return action;
+}
