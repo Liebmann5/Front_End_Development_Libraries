961cad6fc85f7268a037206a40a2910cb84f3778
Nick Liebmann <53795413+Liebmann5@users.noreply.github.com>
2022-11-28 14:07:28 -0600

    Update 5DispatchAction.js

--- Redux/5DispatchAction.js
+++ Redux/5DispatchAction.js
@@ -9,4 +9,5 @@ const loginAction = () => {
 };
 
 // Dispatch the action here:
+// Why do we pass dispatch loginAction() AND NOT loginAction!!????? It's a const??? NOT a ()!! BUT it does = a f(n) that returns an action which makes is = to an obj??
 store.dispatch(loginAction());
